4f68fdab53fa3a34c713b70cb0f6fd087a90a268
mcastellaneta <43062263+mcastellaneta@users.noreply.github.com>
2019-02-14 17:59:38 +0100

    .

---
+++ GamesHub_Code/test/it/gameshub/test/model/UtenteModelTest.java
@@ -0,0 +1,14 @@
+package it.gameshub.test.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UtenteModelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
