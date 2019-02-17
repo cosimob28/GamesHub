5420d46967293afd2477cddc3871f4db549f318b
Domenico Trotta <43494171+domenico97@users.noreply.github.com>
2019-02-17 10:28:48 +0100

    fix GiocoModelTest

--- GamesHub_Code/test/it/gameshub/test/model/GiocoModelTest.java
+++ GamesHub_Code/test/it/gameshub/test/model/GiocoModelTest.java
@@ -7,12 +7,15 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import it.gameshub.bean.Gioco;
 import it.gameshub.bean.Immagine;
+import it.gameshub.bean.Utente;
 import it.gameshub.model.GiocoModel;
 import it.gameshub.model.ImmagineModel;
 
@@ -77,7 +80,25 @@ public class GiocoModelTest {
 	public void testDoRetrieveAll() throws SQLException {
 		ArrayList<Gioco> giochi;
 		giochi = initialize();
-		assertEquals(2, model.doRetrieveAll().size());
+		assertEquals(giochi.size(), model.doRetrieveAll().size());
+		Collection<Gioco> testingCollection = model.doRetrieveAll();
+		
+		Iterator<Gioco> it = testingCollection.iterator();
+		for (int i=0;i<giochi.size();i++) {
+			Gioco x = giochi.get(i);
+			Gioco y = it.next();
+			assertEquals(x.getCode(), y.getCode());
+			assertEquals(x.getAnno(), y.getAnno());
+			assertEquals(x.getDescription(), y.getDescription());
+			assertEquals(x.getGenere(), y.getGenere());
+			assertEquals(x.getName(), y.getName());
+			assertEquals(x.getPegi(), y.getPegi());
+			assertEquals(x.getPiattaforma(), y.getPiattaforma());
+			assertEquals(x.getPrice(), y.getPrice(),0.1);
+			assertEquals(x.getQuantity(), y.getQuantity());
+			assertEquals(x.getVideo(), y.getVideo());
+		}
+		
 	}
 
 	@Test
